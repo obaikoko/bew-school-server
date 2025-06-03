4dac25a7269933187b76de18709b486c3c5f489a
obaikoko <jesseobinna7@gmail.com>
2025-06-03 23:04:02 +0100

    added student data route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoute from './routes/userRoutes';
 import studentRoute from './routes/studentRoutes';
+import dataRoute from './routes/dataRoutes';
 import { notFound, errorHandler } from './middleware/errorMiddleware';
 import cookieParser = require('cookie-parser');
 import cors from 'cors';
@@ -12,8 +13,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 const corsOptions = {
-  origin: 'https://new-school-client.vercel.app',
-  // origin: 'http://localhost:3000',
+  origin: process.env.PUBLIC_DOMAIN,
   credentials: true,
 };
 
@@ -24,6 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/users', userRoute);
 app.use('/api/students', studentRoute);
+app.use('/api/data', dataRoute);
 app.use(errorHandler);
 app.use(notFound);
 app.listen(port, () => console.log(`Server running on port ${port}`));
